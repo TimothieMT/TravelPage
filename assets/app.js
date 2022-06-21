12b687de57e8c384535b5932fc0cf3e42fdac119
Tim Tolk <timtolk@Tims-iMac.fritz.box>
2022-06-21 11:21:25 +0200

    version2.0

--- assets/app.js
+++ assets/app.js
@@ -166,3 +166,11 @@ $(window).resize(function() {
         $('p > br').css({ 'display': 'block' })
     }
 });
+
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+});
